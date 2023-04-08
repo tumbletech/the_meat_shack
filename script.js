c641357b1a88131ded99790e25b2b13a98e23caf
tumbletech <cjjemoralejo@ymail.com>
2023-04-08 17:52:07 +0800

    Still under construction

---
+++ script.js
@@ -0,0 +1,423 @@
+//counter for pork patties//
+function increaseValue01() {
+    var value = parseInt(document.getElementById('quantity-porkpatties').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-porkpatties').value = value;
+}
+
+function decreaseValue01() {
+    var value = parseInt(document.getElementById('quantity-porkpatties').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-porkpatties').value = value;
+}
+
+//counter for pork adobo cut//
+function increaseValue02() {
+    var value = parseInt(document.getElementById('quantity-porkadobocut').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-porkadobocut').value = value;
+}
+
+function decreaseValue02() {
+    var value = parseInt(document.getElementById('quantity-porkadobocut').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-porkadobocut').value = value;
+}
+
+//counter for pork sinigang cut//
+function increaseValue03() {
+    var value = parseInt(document.getElementById('quantity-porksinigangcut').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-porksinigangcut').value = value;
+}
+
+function decreaseValue03() {
+    var value = parseInt(document.getElementById('quantity-porksinigangcut').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-porksinigangcut').value = value;
+}
+
+//counter for pork bbq slices//
+function increaseValue04() {
+    var value = parseInt(document.getElementById('quantity-porkbbqslices').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-porkbbqslices').value = value;
+}
+
+function decreaseValue04() {
+    var value = parseInt(document.getElementById('quantity-porkbbqslices').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-porkbbqslices').value = value;
+}
+
+//counter for pork kasim with skin//
+function increaseValue05() {
+    var value = parseInt(document.getElementById('quantity-porkkasim').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-porkkasim').value = value;
+}
+
+function decreaseValue05() {
+    var value = parseInt(document.getElementById('quantity-porkkasim').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-porkkasim').value = value;
+}
+
+//counter for pork tocino//
+function increaseValue06() {
+    var value = parseInt(document.getElementById('quantity-porktocino').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-porktocino').value = value;
+}
+
+function decreaseValue06() {
+    var value = parseInt(document.getElementById('quantity-porktocino').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-porktocino').value = value;
+}
+
+//counter for ground pork//
+function increaseValue07() {
+    var value = parseInt(document.getElementById('quantity-porkground').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-porkground').value = value;
+}
+
+function decreaseValue07() {
+    var value = parseInt(document.getElementById('quantity-porkground').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-porkground').value = value;
+}
+
+//counter for pork chop//
+function increaseValue08() {
+    var value = parseInt(document.getElementById('quantity-porkchops').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-porkchops').value = value;
+}
+
+function decreaseValue08() {
+    var value = parseInt(document.getElementById('quantity-porkchops').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-porkchops').value = value;
+}
+
+//counter for pork spareribs//
+function increaseValue09() {
+    var value = parseInt(document.getElementById('quantity-porkspareribs').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-porkspareribs').value = value;
+}
+
+function decreaseValue09() {
+    var value = parseInt(document.getElementById('quantity-porkspareribs').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-porkspareribs').value = value;
+}
+
+//counter for beef papaitan//
+function increaseValue10() {
+    var value = parseInt(document.getElementById('quantity-beefpapaitan').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-beefpapaitan').value = value;
+}
+
+function decreaseValue10() {
+    var value = parseInt(document.getElementById('quantity-beefpapaitan').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-beefpapaitan').value = value;
+}
+
+//counter for ground beef//
+function increaseValue11() {
+    var value = parseInt(document.getElementById('quantity-beefground').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-beefground').value = value;
+}
+
+function decreaseValue11() {
+    var value = parseInt(document.getElementById('quantity-beefground').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-beefground').value = value;
+}
+
+//counter for beef tbone steak//
+function increaseValue12() {
+    var value = parseInt(document.getElementById('quantity-beeftbone').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-beeftbone').value = value;
+}
+
+function decreaseValue12() {
+    var value = parseInt(document.getElementById('quantity-beeftbone').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-beeftbone').value = value;
+}
+
+//counter for beef sirloin//
+function increaseValue13() {
+    var value = parseInt(document.getElementById('quantity-beefsirloin').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-beefsirloin').value = value;
+}
+
+function decreaseValue13() {
+    var value = parseInt(document.getElementById('quantity-beefsirloin').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-beefsirloin').value = value;
+}
+
+//counter for beef tapa//
+function increaseValue14() {
+    var value = parseInt(document.getElementById('quantity-beeftapa').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-beeftapa').value = value;
+}
+
+function decreaseValue14() {
+    var value = parseInt(document.getElementById('quantity-beeftapa').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-beeftapa').value = value;
+}
+
+//counter for whole chicken//
+function increaseValue15() {
+    var value = parseInt(document.getElementById('quantity-chickenwhole').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-chickenwhole').value = value;
+}
+
+function decreaseValue15() {
+    var value = parseInt(document.getElementById('quantity-chickenwhole').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-chickenwhole').value = value;
+}
+
+//counter for chicken gizzard//
+function increaseValue16() {
+    var value = parseInt(document.getElementById('quantity-chickengizzard').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-chickengizzard').value = value;
+}
+
+function decreaseValue16() {
+    var value = parseInt(document.getElementById('quantity-chickengizzard').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-chickengizzard').value = value;
+}
+
+//counter for chicken liver//
+function increaseValue17() {
+    var value = parseInt(document.getElementById('quantity-chickenliver').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-chickenliver').value = value;
+}
+
+function decreaseValue17() {
+    var value = parseInt(document.getElementById('quantity-chickenliver').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-chickenliver').value = value;
+}
+
+//counter for chicken breast fillet//
+function increaseValue18() {
+    var value = parseInt(document.getElementById('quantity-chickenbreast').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-chickenbreast').value = value;
+}
+
+function decreaseValue18() {
+    var value = parseInt(document.getElementById('quantity-chickenbreast').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-chickenbreast').value = value;
+}
+
+//counter for chicken wings//
+function increaseValue19() {
+    var value = parseInt(document.getElementById('quantity-chickenwings').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-chickenwings').value = value;
+}
+
+function decreaseValue19() {
+    var value = parseInt(document.getElementById('quantity-chickenwings').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-chickenwings').value = value;
+}
+
+//counter for chicken drumstick//
+function increaseValue20() {
+    var value = parseInt(document.getElementById('quantity-chickendrumstick').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-chickendrumstick').value = value;
+}
+
+function decreaseValue20() {
+    var value = parseInt(document.getElementById('quantity-chickendrumstick').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-chickendrumstick').value = value;
+}
+
+//counter for salmon steak//
+function increaseValue21() {
+    var value = parseInt(document.getElementById('quantity-salmonsteak').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-salmonsteak').value = value;
+}
+
+function decreaseValue21() {
+    var value = parseInt(document.getElementById('quantity-salmonsteak').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-salmonsteak').value = value;
+}
+
+//counter for creamdory//
+function increaseValue22() {
+    var value = parseInt(document.getElementById('quantity-creamdory').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-creamdory').value = value;
+}
+
+function decreaseValue22() {
+    var value = parseInt(document.getElementById('quantity-creamdory').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-creamdory').value = value;
+}
+
+//counter for shrimp//
+function increaseValue23() {
+    var value = parseInt(document.getElementById('quantity-shrimp').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-shrimp').value = value;
+}
+
+function decreaseValue23() {
+    var value = parseInt(document.getElementById('quantity-shrimp').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-shrimp').value = value;
+}
+
+//counter for tuna steak//
+function increaseValue24() {
+    var value = parseInt(document.getElementById('quantity-tunasteak').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-tunasteak').value = value;
+}
+
+function decreaseValue24() {
+    var value = parseInt(document.getElementById('quantity-tunasteak').value, 10)
+ 
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-tunasteak').value = value;
+}
+
+//counter for squidring//
+function increaseValue25() {
+    var value = parseInt(document.getElementById('quantity-squidring').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity-squidring').value = value;
+}
+
+function decreaseValue25() {
+    var value = parseInt(document.getElementById('quantity-squidring').value, 10)
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity-squidring').value = value;
+}
